a5b9877a6f6abb52fe9e11ba78c16d4e2c1fae70
ConfiG <cgytrus@cgyt.ru>
2024-06-27 16:20:17 +0300

    funny mac pch: arm edition

--- src/compiler/clang.rs
+++ src/compiler/clang.rs
@@ -178,6 +178,7 @@ counted_array!(pub static ARGS: [ArgInfo<gcc::ArgData>; _] = [
     take_arg!("-MT", OsString, CanBeSeparated, DepTarget),
     // gross hack to make geode compile with pch on mac :3
     take_arg!("-Xarch_x86_64", OsString, Separated, PreprocessorArgument),
+    take_arg!("-Xarch_arm64", OsString, Separated, PreprocessorArgument),
     take_arg!("-Xclang", OsString, Separated, XClang),
     take_arg!("-add-plugin", OsString, Separated, PassThrough),
     take_arg!("-debug-info-kind", OsString, Concatenated('='), PassThrough),
